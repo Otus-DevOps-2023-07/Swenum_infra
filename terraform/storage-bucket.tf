af4019ae03cd775de799152fc6dade2d3e7335bc
Uladzimir Shchuka <wetoster@gmail.com>
2023-09-11 10:00:56 +0300

    HW7 with ** task

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~>0.95.0"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = var.token_yc
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
+
+resource "yandex_iam_service_account" "sa" {
+  name = "sa-terraform"
+}
+
+// Назначение роли сервисному аккаунту
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = var.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Создание бакета с использованием ключа
+resource "yandex_storage_bucket" "test" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "swenumappterraformstate"
+}
